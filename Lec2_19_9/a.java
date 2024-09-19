33ca005e4b723f232e568fedbda27fe803cc25a6
Dazhyar <dazhyarzanyar@gmail.com>
2024-09-19 10:47:49 +0300

    Init commit

---
+++ Lec2_19_9/a.java
@@ -0,0 +1,11 @@
+public class a {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+
+        p1.x = 10;
+        p1.y = 20;
+
+        System.out.println(p1.x);
+        System.out.println(p1.y);
+    }
+}
