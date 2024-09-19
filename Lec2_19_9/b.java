8ed85b6809f7face19127c23b3c622da69ade2c5
Dazhyar <dazhyarzanyar@gmail.com>
2024-09-19 11:34:18 +0300

    part 2 of the 2nd lecture

--- Lec2_19_9/b.java
+++ Lec2_19_9/b.java
@@ -1,10 +1,11 @@
-public class a {
+public class b {
     public static void main(String[] args) {
-        Point p1 = null;
+        Point p1;
         for (int i = 0; i < 10; i ++) {
             p1 = new Point();
         }
 
-        System.out.println(p1.x);
+        System.out.println(p1); 
+        // java compiler is stupid
     }
 }
